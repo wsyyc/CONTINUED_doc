6bca1b1d5a08602ff12e72c22195131ff996fe1b
wsyyc <40788743+wsyyc@users.noreply.github.com>
2024-08-30 17:27:51 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = '1.0.0'
 
 extensions = [
     'recommonmark',
-    'sphinx_rtd_theme',
+    'sphinx_markdown_tables',
 ]
 
 
@@ -31,11 +31,8 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-master_doc = 'index'
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
